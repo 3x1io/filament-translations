7964b8526cd7ea53588250597cb1e9f892543bda
Pathros Ibarra <patrotsky@yahoo.com>
2022-02-17 12:57:04 -0600

    Add Spanish translations

---
+++ resources/lang/es/translation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "title" => "Traducciones",
+    "label" => "Traducciones",
+    "group" => "Grupo",
+    "key" => "Llave",
+    "text" => "Texto",
+    "scan" => "Búsqueda de nuevos idiomas",
+    "namespace" => "Namespace"
+];
